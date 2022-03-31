cdad4fcfc8c44f49da94152a54ba0e6d391fdbf3
ArlievIslam <arli.islam.555@email.com>
2022-03-31 21:36:15 +0600

    created-project2

---
+++ music_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from music_app.views import *
+
+
+urlpatterns = [
+    path('music/', MusicListView.as_view()),
+    path('music-create/', MusicCreateView.as_view()),
+    path('music-update/<int:pk>/', MusicUpdateView.as_view()),
+    path('music-detail/<int:pk>/', MusicDetailView.as_view()),
+    path('music-delete/<int:pk>/', MusicDeleteView.as_view()),
+
+]
